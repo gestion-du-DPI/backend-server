06117626a7fa1b867bd2234f0ab406511d7d684a
m3kido <63561969+m3kido@users.noreply.github.com>
2025-01-03 16:19:45 +0100

    admin urls

--- GestionDPI/doctor/urls.py
+++ GestionDPI/doctor/urls.py
@@ -11,14 +11,19 @@ from doctor.views import (
     CreatePrescriptionView,
     ArchiveConsultationView,
     GetPrescriptionView,
-    GetDPIView
+    GetDPIView,
+    GetLabImageView,
+    GetRadioImageView,
+    GetRadioObservationView,
+    GetLabObservationView,
+    GetNurseObservationView
 )
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('home',DoctorOnlyView.as_view(),name ='doctor_home'),
-    path('getpatient',GetPatientView.as_view(),name ='get_patient'),
+    path('home', DoctorOnlyView.as_view(), name='doctor_home'),
+    path('getpatient', GetPatientView.as_view(), name='get_patient'),
     path('modifymyuser', ModifyMyUser.as_view(), name='myuser_modify'),
     path('patients', GetPatientsList.as_view(), name='patients_list'),
     path('consultation/create', CreateConultationView.as_view(), name='create_consultation'),
@@ -28,5 +33,10 @@ urlpatterns = [
     path('ticket/create', CreateTicketView.as_view(), name='create_ticket'),
     path('prescription/create', CreatePrescriptionView.as_view(), name='create_prescription'),
     path('prescription/get', GetPrescriptionView.as_view(), name='get_prescription'),
-    path('dpi/get', GetDPIView.as_view(), name='get_dpi')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('dpi/get', GetDPIView.as_view(), name='get_dpi'),
+    path('lab/image/<int:id>', GetLabImageView.as_view(), name='get_lab_image'),
+    path('radio/image/<int:id>', GetRadioImageView.as_view(), name='get_radio_image'),
+    path('radio/observation/<int:id>', GetRadioObservationView.as_view(), name='get_radio_observation'),
+    path('lab/observation/<int:id>', GetLabObservationView.as_view(), name='get_lab_observation'),
+    path('nurse/observation/<int:id>', GetNurseObservationView.as_view(), name='get_nurse_observation')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
